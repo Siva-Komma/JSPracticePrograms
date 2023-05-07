a8cd8a6237366370f764874b8ea3db711ef6c572
Siva-Komma <kommasivajyothi22@gmail.com>
2023-05-07 12:11:58 +0530

    Checking employee doing full time or part time job

--- JSPracticeProbems.js
+++ JSPracticeProbems.js
@@ -9,3 +9,30 @@ if(empCheck==Is_absent)
 else{
     console.log("Employee Present");
 }
+
+//UC2 ability to check Employee full time or part time
+
+const Is_Part_Time=1;
+const Is_Full_Time=2;
+const Part_Time_Hrs=4;
+const Full_Time_Hrs=8;
+const Wage_Per_hrs=20;
+
+{
+    let empHrs=0;
+    let empCheck=0;
+   empCheck =Math.floor(Math.random()*10)%3;
+   switch(empCheck)
+        {
+            case Is_Full_Time:
+                empHrs=Full_Time_Hrs;
+                break;
+            case Is_Part_Time:
+                empHrs=Part_Time_Hrs;
+                break;
+            default:
+                empHrs=0;
+        }
+        let empWage=empHrs*Wage_Per_hrs;
+    console.log("UC2- EmpWage: "+empWage);
+}
